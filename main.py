473c451960581b6cf613b556b2dddd58f4905d92
ANHze <ahmed.nnoteii@gmail.com>
2022-12-31 05:15:46 +0200

    Assignment 5

---
+++ main.py
@@ -0,0 +1,84 @@
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+#
+# for num in numbers:
+#   if num % 5 != 0:
+#     continue
+#   if num > 500:
+#     break
+#   if num > 150:
+#     continue
+#   print(num)
+
+
+
+
+
+# for var in range(10):
+#     print(var)
+# else:
+#     print("Done!")
+
+
+
+
+# num = 76542
+# print(str(num)[::-1])
+
+
+
+
+
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+# x = {}
+# for key in keys:
+#     for value in values:
+#         x[key] = value
+#         values.remove(value)
+#         break
+# print("dictionary is" + str(x))
+
+
+
+
+# sample_dict ={
+#     "name": "Kelly",
+#     "age": 25,
+#     "salary": 8000,
+#     "city": "New york"
+# }
+# keys = ["name", "salary"]
+# for x in keys:
+#     sample_dict.pop(x)
+# print(sample_dict)
+
+
+
+# sample_set = {"Yellow", "Orange", "Black"}
+# sample_list = ["Blue", "Green", "Red"]
+# for x in sample_list:
+#     sample_set.add(x)
+# print(sample_set)
+
+
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+# print(set1.intersection(set2))
+
+
+# tuple1 = (10, 20, 30, 40, 50)
+# new_tuple = tuple1[::-1]
+# print(new_tuple)
+
+
+# tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+# print(tuple1[1][1])
+
+
+# tuple1 = (10, 20, 30, 40)
+# a, b, c, d = tuple1
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
